f4184ebefc01db3dc721bb49505a631c93eb3cdb
Michael DeGori <michael.degori@gmail.com>
2020-12-16 12:22:18 -0500

    heroku

--- models/Booking.js
+++ models/Booking.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Property = require('./Property');
+const User = require('./User');
 
 const bookingSchema = new Schema({
   property: { type: Schema.Types.ObjectId, ref: 'Property' },
+  userFirstName: { type: String, ref: 'User' },
   checkinDate: Date,
   checkoutDate: Date,
 });
--- models/User.js
+++ models/User.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   firstName: String,
   lastName: String,
   birthdate: Date,
+  phone: Number,
   email: String,
   password: String,
 });
 
-module.exports = mongoose.model("User", userSchema, "users");
+module.exports = mongoose.model('User', userSchema, 'users');

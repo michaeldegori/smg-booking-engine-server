5eb0b159ebc1bbb7a822cd0a8fbdb614d3e0fc39
Michael DeGori <michael.degori@gmail.com>
2020-12-16 12:31:52 -0500

    fixed

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const jwt = require('jsonwebtoken');
 const uploader = require('../config/cloudinary-setup');
 
 const authCheck = (req, res, next) => {
-  let token = req.headers?.authorization?.split(' ')[1];
+  let token =
+    req.headers.authorization && req.headers.authorization.split(' ')[1];
 
   if (!token) return res.status(403).send('Unauthenticated');
   const decoded = jwt.verify(token, process.env.token);
